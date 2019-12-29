8443c4509e0413dc329fc7f4770be8429cd8521b
unknown <salmic.jakob@gmail.com>
2019-12-29 13:26:20 +0100

    GUI improvements

--- includes/UserController.js
+++ includes/UserController.js
@@ -20,7 +20,7 @@ var User = require('../models/User');
 
   router.get('/', function (req, res) {
     User.find({}, function (err, users){
-      if(err) return res.status(500).send("There was a problem adding the information to the database. :" + err);
+      if(err) return res.status(500).send("There was a problem getting the information from the database. :" + err);
       res.status(200).send(users);
     })
   });
